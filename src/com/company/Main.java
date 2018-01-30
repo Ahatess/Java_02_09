b114dc45f801b476ce41e9ff1dfc7b8a7633027b
Andrius <andriuscere@gmail.com>
2018-01-30 16:13:35 +0200

    Uzduotis9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	ivedimas();
+	skaiciavimas();
     }
-}
+    static float sanaudos;
+    static float atstumas;
+    public static void ivedimas(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite atstuma KM");
+        atstumas = sc.nextFloat();
+        System.out.println("Įveskite kuro sanaudas L");
+        sanaudos = sc.nextFloat();
+    }
+    public static void skaiciavimas() {
+        float atsakymas = (sanaudos * 100 / atstumas);
+        System.out.println("Jusu vidutines sanaudos: " + atsakymas);
+    }}
